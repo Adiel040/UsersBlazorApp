512bba38f1f967752a015625da05082f32a5dced
Jeancarlos Marizan <jean2656@hotmail.com>
2024-07-08 17:09:38 -0400

    Agregando interfaces

---
+++ UsersBlazorApp.Data/Interfaces/ApiService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UsersBlazorApp.Data.Interfaces
+{
+    public interface ApiService<U>
+    {
+        Task<List<U>> GetAll();
+        Task<U> Get(int id);
+        Task<U> Add(U user);
+        Task<bool> Update(U user);
+        Task<bool> Delete(int id);
+    }
+}
---
+++ UsersBlazorApp.Data/Interfaces/IntServices.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UsersBlazorApp.Data.Interfaces
+{
+    public interface IntServices<I>
+    {
+        Task<List<I>> GetAll();
+        Task<I> Get(int id);
+        Task<I> Add(I user);
+        Task<bool> Update(I user);
+        Task<bool> Delete(int id);
+    }
+}
