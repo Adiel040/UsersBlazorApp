34a924ab41cd9ad5fe15a0936914bc1526ecf7dd
Jeancarlos Marizan <jean2656@hotmail.com>
2024-07-08 17:10:08 -0400

    Agregando claims

---
+++ UserBlazorApp.API/Dto/Claims/RoleClaimsRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace UserBlazorApp.API.Dto.Claims
+{
+    public class RoleClaimsRequest
+    {
+        public int RoleId { get; set; }
+        public string? Claimtype { get; set; }
+        public string? Claimvalue { get; set; }
+    }
+}
---
+++ UserBlazorApp.API/Dto/Claims/RoleClaimsResponse.cs
@@ -0,0 +1,10 @@
+﻿namespace UserBlazorApp.API.Dto.Claims
+{
+    public class RoleClaimsResponse
+    {
+        public int Id { get; set; }
+        public int RoleId { get; set; }
+        public string? Claimtype { get; set; }
+        public string? Claimvalue { get; set; }
+    }
+}
