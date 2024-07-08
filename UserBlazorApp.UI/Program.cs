15c8397546f85bf89e8aaa8564b33897575285b3
Jeancarlos Marizan <jean2656@hotmail.com>
2024-07-08 17:40:29 -0400

    Modificando program del UI

--- UserBlazorApp.UI/Program.cs
+++ UserBlazorApp.UI/Program.cs
@@ -1,11 +1,18 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using UserBlazorApp.UI;
+using UserBlazorApp.UI.Services;
+using UsersBlazorApp.Data.Interfaces;
+using UsersBlazorApp.Data.Models;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("http://localhost:5265") });
+builder.Services.AddScoped<IntServices<AspNetUsers>, UserServices>();
+builder.Services.AddScoped<IntServices<AspNetRoles>, RoleServices>();
+builder.Services.AddScoped<IntServices<AspNetRoleClaims>, ClaimService>();
+
 
 await builder.Build().RunAsync();
