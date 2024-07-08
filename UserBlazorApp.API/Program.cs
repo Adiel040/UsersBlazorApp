a81bd8fd345c1d4aa6fdc751283f7417f3fdee9d
Jeancarlos Marizan <jean2656@hotmail.com>
2024-07-08 17:38:43 -0400

    Modificanco program del Api

--- UserBlazorApp.API/Program.cs
+++ UserBlazorApp.API/Program.cs
@@ -1,6 +1,10 @@
 
 using Microsoft.EntityFrameworkCore;
-using UsersBlazorApp.Data.Context;
+using UserBlazorApp.API.Services;
+using UserBlazorApp.UI.Services;
+using UsersBlazorApp.API.Context;
+using UsersBlazorApp.Data.Interfaces;
+using UsersBlazorApp.Data.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -12,8 +16,21 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 builder.Services.AddDbContext<UsersDbContext>(o => o.UseSqlServer(builder.Configuration.GetConnectionString("ConStr")));
+builder.Services.AddScoped<ApiService<AspNetUsers>, UsuarioServices>();
+builder.Services.AddScoped<ApiService<AspNetRoles>, RoleService>();
+builder.Services.AddScoped<ApiService<AspNetRoleClaims>, ClaimServices>();
+
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAnyOrigin",
+        builder => builder.AllowAnyOrigin()
+                          .AllowAnyMethod()
+                          .AllowAnyHeader());
+});
 
 var app = builder.Build();
+
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -21,6 +38,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+app.UseCors("AllowAnyOrigin");
 
 app.UseHttpsRedirection();
 
