adba6aa9c178a1637af7156dec2113e9627cd6e5
Jeancarlos Marizan <jean2656@hotmail.com>
2024-07-08 17:02:53 -0400

    Modificando roles

--- UsersBlazorApp.Data/Models/AspNetRoles.cs
+++ UsersBlazorApp.Data/Models/AspNetRoles.cs
@@ -14,10 +14,6 @@ public partial class AspNetRoles
     [StringLength(256)]
     public string? Name { get; set; }
 
-    [StringLength(256)]
-    public string? NormalizedName { get; set; }
-
-    public string? ConcurrencyStamp { get; set; }
 
     [InverseProperty("Role")]
     public virtual ICollection<AspNetRoleClaims> AspNetRoleClaims { get; set; } = new List<AspNetRoleClaims>();
@@ -25,4 +21,6 @@ public partial class AspNetRoles
     [ForeignKey("RoleId")]
     [InverseProperty("Role")]
     public virtual ICollection<AspNetUsers> User { get; set; } = new List<AspNetUsers>();
+
+    public ICollection<AspNetRoles> RolesList { get; set; } = new List<AspNetRoles>();
 }
--- UsersBlazorApp.Data/Models/AspNetUsers.cs
+++ UsersBlazorApp.Data/Models/AspNetUsers.cs
@@ -9,50 +9,27 @@ namespace UsersBlazorApp.Data.Models;
 public partial class AspNetUsers
 {
     [Key]
-    public int Id { get; set; }
+    public int UserId { get; set; }
 
     [StringLength(256)]
     public string? UserName { get; set; }
 
     [StringLength(256)]
-    public string? NormalizedUserName { get; set; }
-
-    [StringLength(256)]
     public string? Email { get; set; }
-
-    [StringLength(256)]
-    public string? NormalizedEmail { get; set; }
-
-    public bool EmailConfirmed { get; set; }
 
     public string? PasswordHash { get; set; }
 
-    public string? SecurityStamp { get; set; }
-
-    public string? ConcurrencyStamp { get; set; }
-
     public string? PhoneNumber { get; set; }
 
-    public bool PhoneNumberConfirmed { get; set; }
-
-    public bool TwoFactorEnabled { get; set; }
-
     public DateTimeOffset? LockoutEnd { get; set; }
-
-    public bool LockoutEnabled { get; set; }
-
-    public int AccessFailedCount { get; set; }
 
     [InverseProperty("User")]
     public virtual ICollection<AspNetUserClaims> AspNetUserClaims { get; set; } = new List<AspNetUserClaims>();
 
-    [InverseProperty("User")]
-    public virtual ICollection<AspNetUserLogins> AspNetUserLogins { get; set; } = new List<AspNetUserLogins>();
-
-    [InverseProperty("User")]
-    public virtual ICollection<AspNetUserTokens> AspNetUserTokens { get; set; } = new List<AspNetUserTokens>();
-
     [ForeignKey("UserId")]
     [InverseProperty("User")]
     public virtual ICollection<AspNetRoles> Role { get; set; } = new List<AspNetRoles>();
+
+    [ForeignKey("UserId")]
+    public ICollection<AspNetRoleClaims> UserDetalle {  get; set; } = new List<AspNetRoleClaims>();
 }
