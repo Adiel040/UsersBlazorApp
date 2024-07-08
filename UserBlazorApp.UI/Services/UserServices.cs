0d32fcc9f31cd4ded08d8162e5560bede2695215
Jeancarlos Marizan <jean2656@hotmail.com>
2024-07-08 17:39:59 -0400

    Creando service de Usuarios

---
+++ UserBlazorApp.UI/Services/UserServices.cs
@@ -0,0 +1,69 @@
+﻿using System.Net.Http.Json;
+using UsersBlazorApp.Data.Interfaces;
+using UsersBlazorApp.Data.Models;
+using System.Text.Json;
+
+namespace UserBlazorApp.UI.Services
+{
+    public class UserServices(HttpClient httpClient) : IntServices<AspNetUsers>
+    {
+        public async Task<List<AspNetUsers>> GetAll()
+        {
+            var response = await httpClient.GetAsync("api/AspNetUsers");
+            response.EnsureSuccessStatusCode();
+            return await response.Content.ReadFromJsonAsync<List<AspNetUsers>>();
+        }
+
+        public async Task<AspNetUsers> Get(int id)
+        {
+            var response = await httpClient.GetAsync($"api/AspNetUsers/{id}");
+            response.EnsureSuccessStatusCode();
+            return await response.Content.ReadFromJsonAsync<AspNetUsers>();
+        }
+
+        public async Task<AspNetUsers> Add(AspNetUsers user)
+        {
+            var response = await httpClient.PostAsJsonAsync("api/AspNetUsers", user);
+
+            if (response.IsSuccessStatusCode)
+            {
+                return await response.Content.ReadFromJsonAsync<AspNetUsers>();
+            }
+            else
+            {
+                var errorContent = await response.Content.ReadAsStringAsync();
+                throw new Exception($"Error al agregar usuario: {errorContent}");
+            }
+        }
+
+        public async Task<bool> Update(AspNetUsers user)
+        {
+            var response = await httpClient.PutAsJsonAsync($"api/AspNetUsers/{user.UserId}", user);
+
+            if (response.IsSuccessStatusCode)
+            {
+                return true;
+            }
+            else
+            {
+                var errorContent = await response.Content.ReadAsStringAsync();
+                throw new Exception($"Error al actualizar usuario: {errorContent}");
+            }
+        }
+
+        public async Task<bool> Delete(int id)
+        {
+            var response = await httpClient.DeleteAsync($"api/AspNetUsers/{id}");
+
+            if (response.IsSuccessStatusCode)
+            {
+                return true;
+            }
+            else
+            {
+                var errorContent = await response.Content.ReadAsStringAsync();
+                throw new Exception($"Error al eliminar usuario: {errorContent}");
+            }
+        }
+    }
+}
