91bc3dfc9d1fbc2b43b4776340fa1a3f5a70151b
Jeancarlos Marizan <jean2656@hotmail.com>
2024-07-08 17:10:37 -0400

    Creando Migracion

---
+++ UserBlazorApp.API/Migrations/20240707214709_AgregandoCampos.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace UserBlazorApp.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class AgregandoCampos : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "AspNetRolesId",
+                table: "AspNetRoles",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AspNetRoles_AspNetRolesId",
+                table: "AspNetRoles",
+                column: "AspNetRolesId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_AspNetRoles_AspNetRoles_AspNetRolesId",
+                table: "AspNetRoles",
+                column: "AspNetRolesId",
+                principalTable: "AspNetRoles",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_AspNetRoles_AspNetRoles_AspNetRolesId",
+                table: "AspNetRoles");
+
+            migrationBuilder.DropIndex(
+                name: "IX_AspNetRoles_AspNetRolesId",
+                table: "AspNetRoles");
+
+            migrationBuilder.DropColumn(
+                name: "AspNetRolesId",
+                table: "AspNetRoles");
+        }
+    }
+}
---
+++ UserBlazorApp.API/Migrations/20240707225934_Agregando.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace UserBlazorApp.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class Agregando : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Id",
+                table: "AspNetUsers",
+                newName: "UserId");
+
+            migrationBuilder.AddColumn<int>(
+                name: "UserId",
+                table: "AspNetRoleClaims",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AspNetRoleClaims_UserId",
+                table: "AspNetRoleClaims",
+                column: "UserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_AspNetRoleClaims_AspNetUsers_UserId",
+                table: "AspNetRoleClaims",
+                column: "UserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "UserId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_AspNetRoleClaims_AspNetUsers_UserId",
+                table: "AspNetRoleClaims");
+
+            migrationBuilder.DropIndex(
+                name: "IX_AspNetRoleClaims_UserId",
+                table: "AspNetRoleClaims");
+
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "AspNetRoleClaims");
+
+            migrationBuilder.RenameColumn(
+                name: "UserId",
+                table: "AspNetUsers",
+                newName: "Id");
+        }
+    }
+}
